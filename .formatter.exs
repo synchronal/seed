1fdd1a2dbc7d8f41d969996574071a5993335d50
Erik Hanson <erik@eahanson.com>
2023-03-04 11:52:48 -0800

    [seed] Add `.formatter.exs` and clean up `mix.exs`

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,17 @@
 [
-  import_deps: [:ecto, :ecto_sql, :phoenix],
-  subdirectories: ["priv/*/migrations"],
-  plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
+  import_deps: [:ecto, :moar, :phoenix, :phoenix_html, :phoenix_live_view, :schema_assertions],
+  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{.medic,config,lib,test}/**/*.{ex,exs}", "guides/*.md"],
+  line_length: 120,
+  locals_without_parens: [
+    assert_that: :*,
+    flunk: :*,
+    live_component: :*,
+    live_redirect: :*,
+    on_mount: :*
+  ],
+  markdown: [
+    line_length: 120
+  ],
+  plugins: [Phoenix.LiveView.HTMLFormatter, MarkdownFormatter],
+  subdirectories: ["priv/*/migrations"]
 ]
