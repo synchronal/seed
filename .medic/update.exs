7005ddd17fa219f1ceafc8b42305ebe85bdc52e5
Erik Hanson <erik@eahanson.com>
2023-03-04 11:59:41 -0800

    Don't try to update or build npm

--- .medic/update.exs
+++ .medic/update.exs
@@ -1,9 +1,9 @@
 [
   :update_code,
   :update_mix,
-  :update_npm,
-  # :build_mix,
-  :build_npm,
+  # :update_npm,
+  :build_mix,
+  # :build_npm,
   :doctor,
   :migrate
   # ["Seeding DB", "mix", ["run", "priv/repo/seeds.exs"]]
