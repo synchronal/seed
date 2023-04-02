42f0720f01b5d56593e6f02c3625e42c5cca6b5f
Erik Hanson <erik@eahanson.com>
2023-04-02 05:56:42 -0700

    [seed] Format all markdown files

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,12 @@
 [
   import_deps: [:ecto, :moar, :phoenix, :phoenix_html, :phoenix_live_view, :schema_assertions],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{.medic,config,lib,test}/**/*.{ex,exs}", "guides/*.md"],
+  inputs: [
+    "*.{ex,exs}",
+    "priv/*/seeds.exs",
+    "{.medic,config,lib,test}/**/*.{ex,exs}",
+    "*.md",
+    "guides/*.md"
+  ],
   line_length: 120,
   locals_without_parens: [
     assert_that: :*,
